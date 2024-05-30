c45e8c28d1d6c427bb9740e0bf3d0ddc9868eafe
Iqra Khan <iqraKhan34907@gmail.com>
2024-05-30 18:39:14 +0530

    project

---
+++ v1/application/views/home.php
@@ -0,0 +1,222 @@
+<!-- pre loader area start -->
+
+<main>
+   <section class="tp-hero-6-area tp-hero-6-bg mt--124 p-relative parallax-item fix pt-200 pb-318" id="parallax" data-background="assets/img/icons/vector_smart_object_7.jpg" style="background-size: cover;">
+      <div class="tp-hero-6-overlay shake-horizontal"></div>
+
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-6">
+               <div class="tp-hero-6-content p-relative">
+                  <div class="tp-hero-title-wrapper">
+                     <h2 class="tp-hero-6-title wow fadeInUp" data-wow-duration="3s" data-wow-delay=".6s">Excavate, <br> Innovate, Elevate, <br> And Succeed.</h2>
+                  </div>
+                  <div class="tp-hero-6-button-wrapper d-flex flex-wrap align-items-center wow fadeInUp" data-wow-duration="1s" data-wow-delay=".9s">
+                     <div class="tp-hero-btn">
+                        <a class="tp-btn tp-icon-style pink" href="<?= base_url() ?>#contact">Reach Us<span>
+                              <img src="<?= base_url() ?>assets/img/icons/Vector Smart Object.png" alt="power base" class="our-btn">
+                           </span></a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-6">
+               <div class="tp-hero-6-thumb">
+                  <img class="main wow fadeInRight" data-wow-duration="1s" data-wow-delay=".5s" src="<?= base_url() ?>assets/img/icons/layer_65.png" alt="power base">
+                  <img class="shape-1 wow fadeInRight tilt-shaking" data-wow-duration="1s" data-wow-delay=".9s" src="<?= base_url() ?>assets/img/icons/Layer 60.png" alt="power base">
+                  <!-- <img class="shape-2 wow fadeInRight" data-wow-duration="1s" data-wow-delay="1s" src="assets/img/hero/home-6/hero-6-shape3.png" alt="power base"> -->
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+
+
+   <section class="tp-company-6-area fix p-relative parallax-item pt-90 pb-50" style="background-color: #fff;" id="about">
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-12 justify-content-center text-center">
+               <div class="tp-company-6-wrapper pr-45 wow fadeInUp" data-wow-duration="6s" data-wow-delay=".5s">
+                  <img src="<?= base_url() ?>assets/img/icons/about_us.png" alt="power base" class="mb-10 w-75">
+                  <div class="tp-company-6-title mb-50">
+                     <span class="tp-section-title-pre-2 pink mb-50">About Us</span>
+                     <h3 class="tp-section-title pink">We Specialize in all <br> Types of Deep Foundation Projects</h3>
+                     <p class="othr-p">Our expertise spans a wide range of deep foundation techniques, Our dedicated team of professionals <br> possesses extensive and technical proficiency, ensuring the highest standards of quality <br> and efficiency in every project we undertake.</p>
+                     <a class="tp-btn tp-icon-style pink about-btn" href="<?= base_url() ?>#contact">Reach Us<span>
+                           <img src="<?= base_url() ?>assets/img/icons/Vector Smart Object.png" alt="power base" class="our-btn">
+                        </span></a>
+                  </div>
+               </div>
+            </div>
+
+         </div>
+      </div>
+   </section>
+
+   <section class="tp-project-area tp-project-bottom-color p-relative parallax-item" style="background-color: #fff" id="service">
+
+      <div class="row">
+         <div class="col-lg-6 col-md-6 grid-item cat2 cat4 cat5">
+            <div class="tp-project-item p-relative fix">
+               <div class="tp-project-thumb p-relative fix">
+                  <a href="<?= base_url() ?>">
+                     <img src="<?= base_url() ?>assets/img/icons/image's_.png" alt="power base">
+                  </a>
+               </div>
+            </div>
+         </div>
+
+         <div class="col-lg-6 col-md-6 grid-item cat2 cat4 cat5 othr-box">
+            <img src="<?= base_url() ?>assets/img/icons/Yellow Bar-01.svg" alt="power base" class="lyr-img lyr-img1 shake1">
+            <div class="tp-project-item p-relative fix">
+               <div class="tp-project-thumb p-relative fix">
+                  <h1 class="othr-abt-hdg">A 290</h1>
+                  <h2 class="othr-abt-hdg1">Hydraulic Drill Rig</h2>
+                  <p class="othr-abt-pr">IMT, a global leader in the manufacture of drill rigs <br>
+                     and a brand which has always been synonymous <br>
+                     with quality and reliability in the pursuit of satisfying <br>
+                     market demand, is pleased to present a new <br>
+                     series of A drill rigs. This new line is unique in its <br>
+                     simplicity and flexibility of use, while also perfectly <br>
+                     maintaining sturdiness and productivity. <br>
+                     The A90 drill rig is suitable for the use in several
+                     .</p>
+               </div>
+               <a class="tp-btn tp-icon-style pink about-btn" href="<?= base_url() ?>#contact">Reach Us<span>
+                     <img src="<?= base_url() ?>assets/img/icons/Vector Smart Object.png" alt="power base" class="our-btn">
+                  </span></a>
+            </div>
+         </div>
+
+         <div class="col-lg-6 col-md-6 grid-item cat2 cat4 cat5 d-md-none">
+            <div class="tp-project-item p-relative fix">
+               <div class="tp-project-thumb p-relative fix">
+                  <a href="<?= base_url() ?>l">
+                     <img src="<?= base_url() ?>assets/img/icons/lyr3-image.jpg" alt="power base">
+                  </a>
+               </div>
+            </div>
+         </div>
+
+         <div class="col-lg-6 col-md-6 grid-item cat2 cat4 cat5 othr-box">
+            <img src="<?= base_url() ?>assets/img/icons/Yellow Bar-01.svg" alt="power base" class="lyr-img lyr-img2 shake1">
+            <div class="tp-project-item p-relative fix">
+               <div class="tp-project-thumb p-relative fix">
+                  <h1 class="othr-abt-hdg">AF 460</h1>
+                  <h2 class="othr-abt-hdg1">Hydraulic Drill Rig</h2>
+                  <p class="othr-abt-pr">IMT, a global leader in the manufacture of drill rigs <br>
+                     and a brand which has always been synonymous <br>
+                     with quality and reliability in the pursuit of satisfying <br>
+                     market demand, is pleased to present a new <br>
+                     series of A drill rigs. This new line is unique in its <br>
+                     simplicity and flexibility of use, while also perfectly <br>
+                     maintaining sturdiness and productivity. <br>
+                     The A90 drill rig is suitable for the use in several .</p>
+               </div>
+               <a class="tp-btn tp-icon-style pink about-btn" href="<?= base_url() ?>#contact">Reach Us<span>
+                     <img src="<?= base_url() ?>assets/img/icons/vector_smart_object_4.png" alt="power base" class="our-btn">
+                  </span></a>
+            </div>
+         </div>
+
+         <div class="col-lg-6 col-md-6 grid-item cat2 cat4 cat5 other-d-none">
+            <div class="tp-project-item p-relative fix">
+               <div class="tp-project-thumb p-relative fix">
+                  <a href="<?= base_url() ?>">
+                     <img src="<?= base_url() ?>assets/img/icons/lyr3-image.jpg" alt="power base">
+                  </a>
+               </div>
+            </div>
+         </div>
+
+         <div class="col-lg-6 col-md-6 grid-item cat2 cat4 cat5">
+            <div class="tp-project-item p-relative fix">
+               <div class="tp-project-thumb p-relative fix">
+                  <a href="<?= base_url() ?>">
+                     <img src="<?= base_url() ?>assets/img/icons/Layer-75.png" alt="power base">
+                  </a>
+               </div>
+            </div>
+         </div>
+
+         <div class="col-lg-6 col-md-6 grid-item cat2 cat4 cat5 othr-box">
+            <img src="<?= base_url() ?>assets/img/icons/Yellow Bar-01.svg" alt="power base" class="lyr-img lyr-img3 shake1">
+            <div class="tp-project-item p-relative fix">
+               <div class="tp-project-thumb p-relative fix">
+                  <h1 class="othr-abt-hdg">A 90</h1>
+                  <h2 class="othr-abt-hdg1">Hydraulic Drill Rig</h2>
+                  <p class="othr-abt-pr">our expertise spans a wide range of deep foundation <br>
+                     techniques, Our dedicated team of professionals <br>
+                     possesses extensive experience and technical <br>
+                     proficiency, ensuring the highest standards of quality <br>
+                     and efficiency in every project we undertake.
+                     .</p>
+               </div>
+               <a class="tp-btn tp-icon-style pink about-btn" href="<?= base_url() ?>#contact">Reach Us<span>
+                     <img src="<?= base_url() ?>assets/img/icons/Vector Smart Object.png" alt="power base" class="our-btn">
+                  </span></a>
+            </div>
+         </div>
+
+
+      </div>
+   </section>
+
+   <section class="tp-contact-area p-relative fix pt-190 pb-200 parallax-item">
+      <div class="tp-contact-bg" data-background="assets/img/contact/Layer-78.png"></div>
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-12 text-center">
+               <div class="tp-contact-title-wrapper p-relative wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">
+                  <h3 class="tp-section-title">Join Us in shapping the <br> Future of Mining!</h3>
+               </div>
+               <a class="tp-btn tp-icon-style pink text-white" href="<?= base_url() ?>#contact">Reach Us<span>
+                     <img src="<?= base_url() ?>assets/img/icons/Vector Smart Object.png" alt="power base" class="our-btn">
+                  </span></a>
+            </div>
+
+         </div>
+      </div>
+   </section>
+
+   <!-- contact area start -->
+   <section class="tp-contact-6-area p-relative pt-46 parallax-item">
+
+      <div class="container">
+         <div class="row">
+            <div class="col-xl-8 col-12">
+               <div class="tp-contact-6-box wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
+                  <!-- <h4>Please write to us.</h4> -->
+                  <h3 class="tp-contact-6-title">REACH US</h3>
+                  <div class="tp-contact-6-form">
+                     <div class="row">
+                        <div class="col-lg-3 col-md-6 me-auto">
+                           <input type="text" placeholder="Your name" id="nameInput">
+                        </div>
+                        <div class="col-lg-3 col-md-6 me-auto">
+                           <input type="email" placeholder="Your email" id="emailInput">
+                        </div>
+                        <div class="col-lg-3 col-md-6 me-auto">
+                           <input type="text" placeholder="Address" id="addressInput">
+                        </div>
+                        <!-- <div class="col-lg-3 col-md-6">
+                              <div class="tp-contact-6-btn">
+                                 <button class="tp-btn w-100 othr-btn">Submit Now <span><i class="fa-regular fa-arrow-up-right"></i></span></button>
+                              </div>
+                           </div> -->
+                     </div>
+
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+               <div class="tp-contact-6-btn">
+                  <button class="tp-btn othr-btn">Submit Now <span><i class="fa-regular fa-arrow-up-right"></i></span></button>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <!-- contact area end -->
+</main>
+</div>
---
+++ v1/application/views/includes/footer-link.php
@@ -0,0 +1,27 @@
+<script src="assets/js/vendor/jquery.js"></script>
+<script src="assets/js/bootstrap-bundle.js"></script>
+<script src='assets/js/three.js'></script>
+<script src="assets/js/gsap.js"></script>
+<script src="assets/js/webgl.js"></script>
+<script src="assets/js/hover-effect.umd.js"></script>
+<script src="assets/js/swiper-bundle.js"></script>
+<script src="assets/js/magnific-popup.js"></script>
+<script src="assets/js/tilt.jquery.min.js"></script>
+<script src="assets/js/purecounter.js"></script>
+<script src="assets/js/imagesloaded-pkgd.js"></script>
+<script src="assets/js/isotope-pkgd.js"></script>
+<script src="assets/js/nice-select.js"></script>
+<script src="assets/js/countdown.js"></script>
+<script src="assets/js/wow.js"></script>
+<script src="assets/js/ajax-form.js"></script>
+<script src="assets/js/main.js"></script>
+<script>
+   const parallax = document.getElementById("parallax");
+   window.addEventListener("scroll", function() {
+      let offset = window.pageYOffset;
+      parallax.style.backgroundPositionY = offset * 0.7 + "px";
+   });
+</script>
+</body>
+
+</html>
