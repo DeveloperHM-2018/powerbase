34803dbdf5ac2ab933be6a962f4656678be06c89
harsha <webangeldata@gmail.com>
2024-05-31 23:11:11 +0530

    preloader

--- application/views/includes/footer-link.php
+++ application/views/includes/footer-link.php
@@ -1,20 +1,20 @@
-<script src="assets/js/vendor/jquery.js"></script>
-<script src="assets/js/bootstrap-bundle.js"></script>
-<script src='assets/js/three.js'></script>
-<script src="assets/js/gsap.js"></script>
-<script src="assets/js/webgl.js"></script>
-<script src="assets/js/hover-effect.umd.js"></script>
-<script src="assets/js/swiper-bundle.js"></script>
-<script src="assets/js/magnific-popup.js"></script>
-<script src="assets/js/tilt.jquery.min.js"></script>
-<script src="assets/js/purecounter.js"></script>
-<script src="assets/js/imagesloaded-pkgd.js"></script>
-<script src="assets/js/isotope-pkgd.js"></script>
-<script src="assets/js/nice-select.js"></script>
-<script src="assets/js/countdown.js"></script>
-<script src="assets/js/wow.js"></script>
-<script src="assets/js/ajax-form.js"></script>
-<script src="assets/js/main.js"></script>
+<script src="<?= base_url() ?>assets/js/vendor/jquery.js"></script>
+<script src="<?= base_url() ?>assets/js/bootstrap-bundle.js"></script>
+<script src='<?= base_url() ?>assets/js/three.js'></script>
+<script src="<?= base_url() ?>assets/js/gsap.js"></script>
+<script src="<?= base_url() ?>assets/js/webgl.js"></script>
+<script src="<?= base_url() ?>assets/js/hover-effect.umd.js"></script>
+<script src="<?= base_url() ?>assets/js/swiper-bundle.js"></script>
+<script src="<?= base_url() ?>assets/js/magnific-popup.js"></script>
+<script src="<?= base_url() ?>assets/js/tilt.jquery.min.js"></script>
+<script src="<?= base_url() ?>assets/js/purecounter.js"></script>
+<script src="<?= base_url() ?>assets/js/imagesloaded-pkgd.js"></script>
+<script src="<?= base_url() ?>assets/js/isotope-pkgd.js"></script>
+<script src="<?= base_url() ?>assets/js/nice-select.js"></script>
+<script src="<?= base_url() ?>assets/js/countdown.js"></script>
+<script src="<?= base_url() ?>assets/js/wow.js"></script>
+<script src="<?= base_url() ?>assets/js/ajax-form.js"></script>
+<script src="<?= base_url() ?>assets/js/main.js"></script>
 <script>
    const parallax = document.getElementById("parallax");
    window.addEventListener("scroll", function() {
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,3 +1,23 @@
+
+<div id="loading">
+   <div id="loading-center">
+      <div id="loading-center-absolute">
+         <div class="tp-preloader-content">
+            <div class="tp-preloader-logo">
+               <div class="tp-preloader-circle">
+                  <svg width="190" height="190" viewBox="0 0 380 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                     <circle stroke="#D9D9D9" cx="190" cy="190" r="180" stroke-width="6" stroke-linecap="round">
+                     </circle>
+                     <circle stroke="red" cx="190" cy="190" r="180" stroke-width="6" stroke-linecap="round"></circle>
+                  </svg>
+                  <img src="<?= base_url() ?>assets/img/icons/Logo.png" alt="" style="width:80%">
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
 <div class="back-to-top-wrapper">
    <button id="back_to_top" type="button" class="back-to-top-btn pink">
       <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
